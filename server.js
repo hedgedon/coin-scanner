f281d79a91b7774e89456368603d806cf87a1851
Don Park <donpark@pm.me>
2020-06-07 20:39:22 -0500

    Fetch Nomics API to retrieve /currencies/tickers

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const express = require("express"),
   axios = require("axios");
 
 app.use(cors());
-app.listen(port, () => console.log(`Backend server is live on localhost:${port}`));
 
 // remove favicon error
 app.get('/favicon.ico', (req, res) => res.status(204));
@@ -14,3 +13,30 @@ app.get('/favicon.ico', (req, res) => res.status(204));
 app.get("/", (req, res) => {
   res.send({ message: "Hello World!" });
 });
+
+
+app.use("/api/nomics/currencies/ticker", async (req, res) => {
+  const key = `${process.env.NOMICS_API_KEY}`;
+  const config = {
+    params: {
+      key: key
+    }
+  };
+
+  try {
+    const resp = await axios.get('https://api.nomics.com/v1/currencies/ticker', config)
+      .then(response => {
+        const { data } = response;
+        const coins = data;
+        const coinsList = coins.slice([0], [10]).map((item, i) => {
+          console.log(item);
+          return item;
+        });
+        res.send(coinsList)
+      })
+  } catch (error) {
+    console.error("Your ERROR: ", error)
+  }
+})
+      
+app.listen(port, () => console.log(`Backend server is live on localhost:${port}`));
