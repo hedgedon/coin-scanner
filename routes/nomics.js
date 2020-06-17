22e47969baa202f2f90f3cc50b8197999a436d1c
Don Park <donpark@pm.me>
2020-06-16 19:24:25 -0500

    minor refactor

--- routes/nomics.js
+++ routes/nomics.js
@@ -3,19 +3,19 @@ axios = require("axios");
 const router = express.Router()
 
 router.get("/currencies/ticker", async (req, res) => {
+  const baseUrl = 'https://api.nomics.com/v1';
   const key = `${process.env.NOMICS_API_KEY}`;
   const config = {
     params: {
       key: key
     }
   };
-  const baseUrl = 'https://api.nomics.com/v1';
 
   try {
     const response = await axios.get(`${baseUrl}/currencies/ticker`, config)
     let data = await response.data
     const coins = data
-    const coinsList = coins.slice([0], [10]).map((item, i) => {
+    const coinsList = coins.slice([0], [10]).map((item) => {
       // console.log(item);
       return item;
     });
