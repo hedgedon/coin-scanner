088f838928cfb818e49e618959b9177bd20bb23c
Don Park <donpark@pm.me>
2020-06-20 22:44:27 -0500

    conditional rendering of data table

---
+++ client/src/api/index.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const url = 'http://localhost:5000/nomics/currencies/ticker';
+
+export const fetchData = async () => {
+  try {
+    const { data } = await axios.get(url);
+    // this returns another array of our objects. 
+    // return { data }
+    
+    // Instant return of an object instead.
+    const modifiedData = data.map((coins) => {
+      // console.log(coins) //  ===> 0: {id: "BTC", currency: "BTC", symbol: "BTC", name: "Bitcoin", … }
+      return coins
+    })
+
+    return modifiedData;
+  } catch (error) {
+    console.error("Your ERROR", error)
+  }
+}
